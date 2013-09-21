7e356e986b207bd02664b2a7ca0d03b0b6ddf3f6
rick carlino <rick.carlino@gmail.com>
2013-09-20 23:58:52 -0700

    Up and running. Does not work on multiple recordings

--- src/sandbox3.coffee
+++ src/sandbox3.coffee
@@ -1,16 +1,20 @@
 class Recorder
   constructor: ->
     navigator.getUserMedia = navigator.getUserMedia or navigator.webkitGetUserMedia or navigator.mozGetUserMedia or navigator.msGetUserMedia  unless navigator.getUserMedia
-    #This needsto be refactored. Keep losing scope to it when I call record.onaudioprcess()
-    @recording    = no
-    @leftchannel  = []
-    @rightchannel = []
-    @recordingLength = 0
     @sampleRate = 44100
+    @setNastyGlobals()
     if navigator.getUserMedia
       navigator.getUserMedia  {audio: true} , @sample, @failure
     else
       alert "Your browser doesn't support WebAudio or WebRTC. Upgrade to the latest Chrome or Firefox."
+  setNastyGlobals: ->
+    #This needs refactored. 
+    # Qucikfix because I keep scope of these when I call record.onaudioprcess()
+    window.__recording    = no
+    window.__leftchannel  = []
+    window.__rightchannel = []
+    window.__recordingLength = 0
+    null
   sample: (current_stream) ->
     AudioCtx = window.AudioContext or window.webkitAudioContext
     context = new AudioCtx()
@@ -33,9 +37,9 @@ class Recorder
       right = current_stream.inputBuffer.getChannelData(1)
       
       # we clone the samples
-      @leftchannel.push new Float32Array(left)
-      @rightchannel.push new Float32Array(right)
-      @recordingLength += bufferSize
+      window.__leftchannel.push new Float32Array(left)
+      window.__rightchannel.push new Float32Array(right)
+      window.__recordingLength += bufferSize
     console.log 'below'
 
     # we connect the recorder
@@ -47,16 +51,16 @@ class Recorder
     window.__recording = yes
 
     # reset the buffers for the new recording
-    @leftchannel.length = @rightchannel.length = 0
-    @recordingLength = 0
+    window.__leftchannel.length = window.__rightchannel.length = 0
+    window.__recordingLength = 0
     console.log "Recording now..."
   stop: ->
     # we stop recording
-    window.__recording = false
+    recording = no
 
     # we flat the left and right channels down
-    leftBuffer  = @_mergeBuffers(@leftchannel, @recordingLength)
-    rightBuffer = @_mergeBuffers(@rightchannel, @recordingLength)
+    leftBuffer  = @_mergeBuffers(window.__leftchannel, window.__recordingLength)
+    rightBuffer = @_mergeBuffers(window.__rightchannel, window.__recordingLength)
 
     # we interleave both channels together
     interleaved = @_interleave(leftBuffer, rightBuffer)
@@ -64,27 +68,21 @@ class Recorder
     # we create our wav file
     buffer = new ArrayBuffer(44 + interleaved.length * 2)
     view = new DataView(buffer)
-    console.log 'A'
-    console.log buffer.byteLength
-    console.log view.byteLength
+
     # RIFF chunk descriptor
     @_writeUTFBytes view, 0, "RIFF"
     view.setUint32 4, 44 + interleaved.length * 2, true
     @_writeUTFBytes view, 8, "WAVE"
-    console.log 'B'
-    console.log buffer.byteLength
-    console.log view.byteLength
+
     # FMT sub-chunk
     @_writeUTFBytes view, 12, "fmt "
     view.setUint32 16, 16, true
     view.setUint16 20, 1, true
-    console.log 'C'
-    console.log buffer.byteLength
-    console.log view.byteLength
+
     # stereo (2 channels)
     view.setUint16 22, 2, true
-    view.setUint32 24, @sampleRate, true
-    view.setUint32 28, @sampleRate * 4, true
+    view.setUint32 24, window.__sampleRate, true
+    view.setUint32 28, window.__sampleRate * 4, true
     view.setUint16 32, 4, true
     view.setUint16 34, 16, true
 
@@ -108,6 +106,14 @@ class Recorder
       type: "audio/wav"
     )
     @url = (window.URL || window.webkitURL).createObjectURL(@blob)
+    @setNastyGlobals()
+  download: (fileName = 'output.wav') ->
+    link = window.document.createElement("a")
+    link.href = @url
+    link.download = fileName
+    click = document.createEvent("Event")
+    click.initEvent "click", true, true
+    link.dispatchEvent click
 
   _interleave: (leftChannel, rightChannel) ->
     length = leftChannel.length + rightChannel.length
--- src/sandbox3.js
+++ src/sandbox3.js
@@ -7,11 +7,8 @@
       if (!navigator.getUserMedia) {
         navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
       }
-      this.recording = false;
-      this.leftchannel = [];
-      this.rightchannel = [];
-      this.recordingLength = 0;
       this.sampleRate = 44100;
+      this.setNastyGlobals();
       if (navigator.getUserMedia) {
         navigator.getUserMedia({
           audio: true
@@ -20,6 +17,14 @@
         alert("Your browser doesn't support WebAudio or WebRTC. Upgrade to the latest Chrome or Firefox.");
       }
     }
+
+    Recorder.prototype.setNastyGlobals = function() {
+      window.__recording = false;
+      window.__leftchannel = [];
+      window.__rightchannel = [];
+      window.__recordingLength = 0;
+      return null;
+    };
 
     Recorder.prototype.sample = function(current_stream) {
       var AudioCtx, audioInput, bufferSize, context, recorder, volume;
@@ -44,9 +49,9 @@
         }
         left = current_stream.inputBuffer.getChannelData(0);
         right = current_stream.inputBuffer.getChannelData(1);
-        this.leftchannel.push(new Float32Array(left));
-        this.rightchannel.push(new Float32Array(right));
-        return this.recordingLength += bufferSize;
+        window.__leftchannel.push(new Float32Array(left));
+        window.__rightchannel.push(new Float32Array(right));
+        return window.__recordingLength += bufferSize;
       };
       console.log('below');
       volume.connect(recorder);
@@ -56,37 +61,28 @@
 
     Recorder.prototype.start = function() {
       window.__recording = true;
-      this.leftchannel.length = this.rightchannel.length = 0;
-      this.recordingLength = 0;
+      window.__leftchannel.length = window.__rightchannel.length = 0;
+      window.__recordingLength = 0;
       return console.log("Recording now...");
     };
 
     Recorder.prototype.stop = function() {
-      var buffer, i, index, interleaved, leftBuffer, lng, rightBuffer, view, volume;
-      window.__recording = false;
-      leftBuffer = this._mergeBuffers(this.leftchannel, this.recordingLength);
-      rightBuffer = this._mergeBuffers(this.rightchannel, this.recordingLength);
+      var buffer, i, index, interleaved, leftBuffer, lng, recording, rightBuffer, view, volume;
+      recording = false;
+      leftBuffer = this._mergeBuffers(window.__leftchannel, window.__recordingLength);
+      rightBuffer = this._mergeBuffers(window.__rightchannel, window.__recordingLength);
       interleaved = this._interleave(leftBuffer, rightBuffer);
       buffer = new ArrayBuffer(44 + interleaved.length * 2);
       view = new DataView(buffer);
-      console.log('A');
-      console.log(buffer.byteLength);
-      console.log(view.byteLength);
       this._writeUTFBytes(view, 0, "RIFF");
       view.setUint32(4, 44 + interleaved.length * 2, true);
       this._writeUTFBytes(view, 8, "WAVE");
-      console.log('B');
-      console.log(buffer.byteLength);
-      console.log(view.byteLength);
       this._writeUTFBytes(view, 12, "fmt ");
       view.setUint32(16, 16, true);
       view.setUint16(20, 1, true);
-      console.log('C');
-      console.log(buffer.byteLength);
-      console.log(view.byteLength);
       view.setUint16(22, 2, true);
-      view.setUint32(24, this.sampleRate, true);
-      view.setUint32(28, this.sampleRate * 4, true);
+      view.setUint32(24, window.__sampleRate, true);
+      view.setUint32(28, window.__sampleRate * 4, true);
       view.setUint16(32, 4, true);
       view.setUint16(34, 16, true);
       this._writeUTFBytes(view, 36, "data");
@@ -103,7 +99,21 @@
       this.blob = new Blob([view], {
         type: "audio/wav"
       });
-      return this.url = (window.URL || window.webkitURL).createObjectURL(this.blob);
+      this.url = (window.URL || window.webkitURL).createObjectURL(this.blob);
+      return this.setNastyGlobals();
+    };
+
+    Recorder.prototype.download = function(fileName) {
+      var click, link;
+      if (fileName == null) {
+        fileName = 'output.wav';
+      }
+      link = window.document.createElement("a");
+      link.href = this.url;
+      link.download = fileName;
+      click = document.createEvent("Event");
+      click.initEvent("click", true, true);
+      return link.dispatchEvent(click);
     };
 
     Recorder.prototype._interleave = function(leftChannel, rightChannel) {
